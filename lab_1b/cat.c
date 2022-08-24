bb0aec17d2c9ab78a1bb22f23980519b2308c38e
Balbachan <balbachanlaura@gmail.com>
2022-08-24 15:58:20 -0300

    Começo - Comando cat

---
+++ lab_1b/cat.c
@@ -0,0 +1,16 @@
+/*
+Sistemas Operacionais 
+Laboratório 2 - Execício 1
+
+Alunos:
+Laura Carolina Balbachan dos Santos (32173008)
+Tiago Caspirro Cardoso (32165978)
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]){
+    
+}
