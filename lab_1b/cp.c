eb0c711de4949129d1c370a129ee0e43ebdb01fa
Balbachan <balbachanlaura@gmail.com>
2022-08-25 09:08:46 -0300

    Update cp.c

--- lab_1b/cp.c
+++ lab_1b/cp.c
@@ -15,20 +15,30 @@ int main(int argc, char *argv[]){
     FILE *fileptr1, *fileptr2;
 
     if((fileptr1 = fopen(argv[1], "r")) == NULL){ 
-        printf("Arquivo não encontrado!");
+        printf("\nArquivo %s não encontrado!", argv[1]);
         exit(1);
     }
     
-    fileptr2 = fopen(argv[2], "w");
+    if((fileptr2 = fopen(argv[2], "r+")) != NULL){
+        printf("\nNão foi possível criar o arquivo %s.", argv[2]);
+        exit(1);
+    }
+    else{
+        printf("\nArquivo %s criado com sucesso!", argv[2]);
+    }
+    
     
     while(1){ 
         letras = fgetc(fileptr1);
-        if(letras == EOF){
+        if(letras != EOF){
+            fprintf(fileptr2, letras);
+        }
+        else{ 
             break;
         }
-        printf("%c", letras);
     }
 
     fclose(fileptr1);
+    fclose(fileptr2);
     return 0;
 }
