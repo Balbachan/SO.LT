ac4eda9c17c78f23bd07fe22d3ace5b2428e9d7c
Balbachan <balbachanlaura@gmail.com>
2022-08-25 01:42:56 -0300

    Update cp.c

--- lab_1b/cp.c
+++ lab_1b/cp.c
@@ -8,3 +8,28 @@ Tiago Caspirro Cardoso (32165978)
 
 */
 
+#include <stdio.h>
+
+int main(int argc, char *argv1[], char *argv2[]){
+    char letras;
+    FILE *fileptr1, *fileptr2;
+
+    if((fileptr1 = fopen(argv1[1], "r")) == NULL){ 
+        printf("Arquivo não encontrado!");
+        exit(1);
+    }
+    else{
+        fileptr2 = fopen(argv2[1], "w");
+    }
+
+    while(1){ 
+        letras = fgetc(fileptr1);
+        if(letras == EOF){
+            break;
+        }
+        printf("%c", letras);
+    }
+
+    fclose(fileptr1);
+    return 0;
+}
